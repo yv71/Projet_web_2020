26299178123e9410d3aa7cdb750aaad15c0a0172
Elie Isaac <elie.isaac@laposte.net>
2020-12-26 23:02:10 +0100

    modif emplacmeents tests

--- projet-web-gr2/e2e/tremie.js
+++
@@ -1,8 +0,0 @@
-// test pour tremie
-describe('Protractor Demo App', function () {
-    it('should have a title', function () {
-        browser.get('http://juliemr.github.io/protractor-demo/');
-
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
-});
