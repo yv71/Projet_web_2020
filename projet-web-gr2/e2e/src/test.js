4e06ab5a77f6744b9f8da28206e15e81a22b6e72
AlexianeIUT <35493278+AlexianeIUT@users.noreply.github.com>
2020-12-31 15:46:07 +0100

    test
    fonctionne les tests tremie vide, boisseau vide et cellule vide

--- projet-web-gr2/e2e/src/test.js
+++ projet-web-gr2/e2e/src/test.js
@@ -8,7 +8,6 @@ describe("Test", function() {
   var infos = element(by.css('.menu')).element(by.css('.infosGenerales')).all(by.css('p')).first();
   var type = element(by.css('.menu')).element(by.css('.infosGenerales')).element(by.css('h4')).element(by.css('.type'));
   var buttonMenu = element(by.css('.menu')).element(by.css('.btnsActions')).element(by.css('button'));
-  var a = 'True'
 
   beforeAll(function() {
     jasmine.DEFAULT_TIMEOUT_INTERVAL= 600000;
@@ -18,52 +17,7 @@ describe("Test", function() {
     browser.get(url);
   });
 //////////////////////////// test affichage menu ///////////////////////////////////
-    
-    //// Objet plein ///////
-  // Test pour voir si le menu de la trémie s'affiche avec les informations du lot
-  it('should click on a "trémie"', function(){
-    console.log('Test trémie pleine');
-    browser.manage().timeouts().implicitlyWait(55000)
-    browser.sleep(2000);
-    tremie.click();
-    browser.sleep(1000);
-    var type = element(by.css('.menu')).element(by.css('.infosGenerales')).element(by.css('h4')).element(by.css('.type'));
-    expect(type.getText()).toBe('Trémie');
-  });
-
-  // Test pour voir si le menu du boisseau s'affiche avec les informations du lot  
-  it('should click on a "boisseau"', function(){
-    console.log('Test boisseau plein');
-    browser.manage().timeouts().implicitlyWait(55000)
-    browser.sleep(2000);
-    boisseau.click();
-    browser.sleep(1000);
-    var type = element(by.css('.menu')).element(by.css('.infosGenerales')).element(by.css('h4')).element(by.css('.type'));
-    expect(type.getText()).toBe('Boisseau');
-  });
-
-  // Test pour voir si le menu de la cellule s'affiche avec les informations du lot
-  it('should click on a "cellule"', function(){
-    console.log('Test cellule pleine');
-    browser.manage().timeouts().implicitlyWait(55000)
-    browser.sleep(2000);
-    cellule.click();
-    browser.sleep(1000);
-    var type = element(by.css('.menu')).element(by.css('.infosGenerales')).element(by.css('h4')).element(by.css('.type'));
-    expect(type.getText()).toBe('Cellule');
-  });
-
-  // Test pour voir si le menu du camion s'affiche avec les informations du lot
-  it('should click on a "camion"', function(){
-    browser.manage().timeouts().implicitlyWait(55000)
-    browser.sleep(2000);
-    camion.click();
-    browser.sleep(1000);
-    var type = element(by.css('.menu')).element(by.css('.infosGenerales')).element(by.css('h4')).element(by.css('.type'));
-    expect(type.getText()).toBe('Camion');
-});
-
-  /////Objet vide /////////
+     /////Objet vide /////////
     
   // Test pour vérifier qu'il ne se passe rien quand on clic sur le bouton du menu de la trémie 
   it('should do nothing if "trémie" is empty', function(){
@@ -74,11 +28,11 @@ describe("Test", function() {
     browser.sleep(1000);
     buttonMenu.click();
     browser.sleep(2000);
-    expect().nothing();
+    expect(infos.getText()).toContain('Pas de lot contenu');
   });
 
-
-  it('should click on a "cellule"', function(){
+// Test pour vérifier qu'il ne se passe rien quand on clic sur le bouton du menu de la cellule
+  it('shoulddo nothing if "cellule" is empty', function(){
     console.log('Test cellule vide');
     browser.manage().timeouts().implicitlyWait(55000)
     browser.sleep(2000);
@@ -86,20 +40,9 @@ describe("Test", function() {
     browser.sleep(1000);
     buttonMenu.click();
     browser.sleep(2000);
-    expect(type.getText()).toBe('Cellule');
+    expect(infos.getText()).toContain('Pas de lot contenu');
   });
-
-  //Je suis pas sur pour ce test vu que le bouton envoyer au client n'apparait pas de suite 
-  // Test pour vérifier qu'il ne se passe rien 
-  it('should do nothing if "camion" is empty', function(){
-    console.log('Test camion vide');
-    browser.manage().timeouts().implicitlyWait(55000)
-    browser.sleep(2000);
-    camion.click();
-    browser.sleep(1000);
-    expect().nothing();
-  });
-
+  
   // Test pour vérifier qu'il ne se passe rien quand on clic sur le bouton du menu du boisseau
   it('should do nothing if "boisseau" is empty', function(){
     console.log('Test boisseau vide');
@@ -107,9 +50,71 @@ describe("Test", function() {
     browser.sleep(2000);
     boisseau.click();
     browser.sleep(1000);
-    expect().nothing();
+    buttonMenu.click();
+    browser.sleep(2000);
+    expect(infos.getText()).toContain('Pas de lot contenu');
+  });
+  
+  // Test pour vérifier qu'il ne se passe rien 
+  it('should do nothing if "camion" is empty', function(){
+    console.log('Test camion vide');
+    browser.manage().timeouts().implicitlyWait(55000)
+    browser.sleep(2000);
+    camion.click();
+    browser.sleep(2000);
+    expect(infos.getText()).toContain('Au silo');
   });
 
+  //// Objet plein ///////
+  // Test pour voir si le menu de la trémie s'affiche avec les informations du lot
+  it('should click on a "trémie"', function(){
+    console.log('Test trémie pleine');
+    browser.manage().timeouts().implicitlyWait(55000)
+    browser.sleep(2000);
+    tremie.click();
+    browser.sleep(1000);
+    browser.switchTo().alert().accept();
+    browser.sleep(1000);
+    expect(type.getText()).toBe('Trémie');
+    //expect(type.getText()).toContain(infos);
+  });
+
+   // Test pour voir si le menu de la cellule s'affiche avec les informations du lot
+  it('should click on a "cellule"', function(){
+    console.log('Test cellule pleine');
+    browser.manage().timeouts().implicitlyWait(55000)
+    browser.sleep(2000);
+    cellule.click();
+    browser.sleep(1000);
+    buttonMenu.click();
+    browser.switchTo().alert().accept();
+    browser.sleep(1000);
+    expect(type.getText()).toBe('Cellule');
+  });
+
+  // Test pour voir si le menu du boisseau s'affiche avec les informations du lot  
+  it('should click on a "boisseau"', function(){
+    console.log('Test boisseau plein');
+    browser.manage().timeouts().implicitlyWait(55000)
+    browser.sleep(2000);
+    boisseau.click();
+    browser.sleep(1000);
+    browser.switchTo().alert().accept();
+    browser.sleep(1000);
+    expect(type.getText()).toBe('Boisseau');
+  });
+
+  // Test pour voir si le menu du camion s'affiche avec les informations du lot
+  it('should click on a "camion"', function(){
+    console.log('Test camion plein');
+    browser.manage().timeouts().implicitlyWait(55000)
+    browser.sleep(2000);
+    camion.click();
+    browser.sleep(1000);
+    browser.switchTo().alert().accept();
+    browser.sleep(1000);
+    expect(type.getText()).toBe('Camion');
+  });
 
   ////Déplacement du lot //////
 
@@ -139,9 +144,9 @@ describe("Test", function() {
     expect(infos.getText()).toContain('Lot contenu :');
   });
 
-  //Test pour voir si le lot se déplace bien de la trémie à la cellule 
+  //Test pour voir si le lot se déplace bien de la cellule au boisseau 
   it('should send the "lot" to "boisseau" if "cellule" is full', function(){
-    console.log('Test déplace');
+    console.log('Test déplacement cellule à boisseau');
     browser.manage().timeouts().implicitlyWait(70000)
     browser.sleep(2000);
     /*setInterval( () => {
@@ -165,6 +170,7 @@ describe("Test", function() {
     expect(infos.getText()).toContain('Lot contenu :');
   });
 
+  // test alerte si il n'existe pas de cellule de taille suffisante pour le lot 
   it('should alert that there is no "cellule" empty', function(){
     console.log('Test 5');
     browser.manage().timeouts().implicitlyWait(200000);
